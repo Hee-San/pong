709b18095f4fc3195371d16afe75f2d3d1a12ce9
へーさん <hira9603859504@gmail.com>
2016-09-23 23:44:07 +0900

    パドル作成

---
+++ Assets/Scriptes/Paddle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	public float paddleSpeed = 1;
+	public Vector3 playerPos;
+	
+	void Update () {
+		float yPos = transform.position.y + (Input.GetAxis ("Vertical") * paddleSpeed);
+		Vector3 playerPos = new Vector3 (-2.5f, Mathf.Clamp (yPos, -1.45f, 1.45f), 0);
+		transform.position = playerPos;
+	}
+}

23d0c6e554c0d5a7c38943682f4d222dee6d9992
へーさん <hira9603859504@gmail.com>
2016-09-25 23:00:04 +0900

    敵実装・AI

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed = 8;
+	Vector3 targetPos;
+	GameObject ballObj;
+
+	void Start () {
+		ballObj = GameObject.FindGameObjectWithTag("Ball");
+	}
+	
+	void Update () {
+		targetPos = Vector3.Lerp (gameObject.transform.position, ballObj.transform.position, Time.deltaTime * speed);
+		gameObject.transform.position = new Vector3 (2.5f, Mathf.Clamp (targetPos.y, -1.55f, 1.55f), 0);
+	}
+}
